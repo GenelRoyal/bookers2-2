e71c14371187c1fbe1938e4d5953d9c86b1ba898
EC2 Default User <ec2-user@ip-172-31-16-243.ap-northeast-1.compute.internal>
2024-10-05 10:56:38 +0000

    [Finish] 8c

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,7 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :chats, only: [:show, :create]
-  resources :groups, only: [:new, :index, :show, :create, :edit, :update]
+  resources :groups, only: [:new, :index, :show, :create, :edit, :update] do
+    resource :group_users, only: [:create, :destroy]
+  end
 end
